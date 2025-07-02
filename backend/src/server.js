156042d7feadc04d1aa6f8015f38a9faa99998d4
MirazZim <miraz.zim.38@gmail.com>
2025-07-03 00:07:02 +0600

    Organized our codeBase 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv/config.js";
-import { sql } from "./config/db.js";
+import { initDB, sql } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 
@@ -13,30 +13,6 @@ app.use(express.json());
 const PORT = process.env.PORT || 8001;
 
 
-async function initDB() {
-    try {
-        await sql`
-            CREATE TABLE IF NOT EXISTS transactions(
-                id SERIAL PRIMARY KEY,
-                user_id VARCHAR(255) NOT NULL,
-                title VARCHAR(255) NOT NULL,
-                amount DECIMAL(10,2) NOT NULL,
-                category VARCHAR(255) NOT NULL,
-                created_at DATE NOT NULL DEFAULT CURRENT_DATE
-            )
-        `
-
-        console.log("Database initialized successfully");
-    } catch (error) {
-        console.error("Error initializing database:", error);
-        process.exit(1); // STATUS CODE 1 MEANS ERROR AND 0 MEANS SUCCESS
-    }
-}
-
-//DECIMAL (10,2)
-//MEANS 10 DIGITS IN TOTAL AND 2 DIGITS AFTER THE DECIMAL
-//SO 123456789.12 IS VALID
-//BUT 12345678912 IS NOT VALID
 
 app.use("/api/transactions", transactionsRoute);
 
